3ba9f89ae9d836430f6c319655489571f6a516c1
6O0b <58083333+6O0b@users.noreply.github.com>
2019-12-14 18:03:56 +0900

    Add files via upload

---
+++ Code/Scheduler.R
@@ -0,0 +1,15 @@
+# install.packages("lubridate")
+# install.packages("taskscheduleR")
+library(lubridate)
+library(taskscheduleR)
+
+dir <- 'D:/김종원/대학교/2학년/2학기/데이터 시각화/62/code/GetData.R'
+
+# 스케줄 생성 
+taskscheduler_create(taskname = 'LOL_Data', rscript = dir,
+                     schedule = 'DAILY',
+                     starttime = "07:00",
+                     startdate = format(Sys.Date(), '%Y/%m/%d'))
+
+# 스케줄 삭제
+taskscheduler_delete('LOL_Data')
